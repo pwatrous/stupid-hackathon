b00973a3e5c587933c4b820e7527b1a1f2315bce
Samuel S. Siegmeister <siegmeister.s@husky.neu.edu>
2018-04-07 18:00:25 -0400

    Added menu function

--- client/app/components/Order/index.js
+++ client/app/components/Order/index.js
@@ -1,94 +1,115 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 import '../../styles/order.scss';
 
 class Order extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            streetAddress: "",
-            cityStateZip: "",
-            restaurant: {},
-            hasErrors: false,
-            errorMessage: ""
-        };
-    }
-    
-    handleChange = name => event => {
-        this.setState({
-            [name]: event.target.value,
-        });
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      streetAddress: "",
+      cityStateZip: "",
+      restaurant: {},
+      hasErrors: false,
+      errorMessage: ""
     };
+  }
 
-    handleSubmit = () => {
-        this.getRestaurants();
-        // this.getMenu();
-        // this.placeOrder();
-    }
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
+  };
 
-    getRestaurants = () => {
-        //TODO validate inputs before making fetch request
-        const { streetAddress, cityStateZip } = this.state;
-        this.setState({ hasErrors: false, errorMessage: "" });
-        let address = `${streetAddress} ${cityStateZip}`.split(" ").join("+");
-        let baseUrl = 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=delivery&street-address=';
-        fetch(baseUrl + address,
-            {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-Access-Token': '2f6313388bf0a784' // replace with process.env.API_KEY when it works lol
-                },
+  handleSubmit = () => {
+    this.getRestaurants();
+    // this.getMenu();
+    // this.placeOrder();
+  }
 
-            }
-        )
-            .then(data => data.json())
-            .then(response => {
-                let index = Math.round(Math.random() * response.restaurants.length);
-                this.setState({ restaurant: response.restaurants[index] });
-            })
-            .catch(err => {
-                console.log(err)
-                this.setState({
-                    hasErrors: true,
-                    errorMessage: "Couldn't fetch restaurants"
-                });
-            });
+  getRestaurants = () => {
+    //TODO validate inputs before making fetch request
+    const {streetAddress, cityStateZip} = this.state;
+    this.setState({hasErrors: false, errorMessage: ""});
+    let address = `${streetAddress} ${cityStateZip}`.split(" ").join("+");
+    let baseUrl = 'https://api.eatstreet.com/publicapi/v1/restaurant/search?method=delivery&street-address=';
+    fetch(baseUrl + address,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Access-Token': '2f6313388bf0a784' // replace with process.env.API_KEY when it works lol
+        },
 
-    }
+      }
+    )
+      .then(data => data.json())
+      .then(response => {
+        let index = Math.round(Math.random() * response.restaurants.length);
+        console.log(response.restaurants[index]);
+        this.setState({restaurant: response.restaurants[index]});
+        this.getMenu();
+      })
+      .catch(err => {
+        console.log(err)
+        this.setState({
+          hasErrors: true,
+          errorMessage: "Couldn't fetch restaurants"
+        });
+      });
 
-    render() {
-        return (
-            <div>
-                <form className="order-form" noValidate autoComplete="off">
-                    <TextField
-                        required
-                        id="streetAddress"
-                        label="Street Address"
-                        margin="normal"
-                        onChange={this.handleChange('streetAddress')}
-                    />
-                    <TextField
-                        required
-                        id="cityStateZip"
-                        label="City, State, Zip Code"
-                        margin="normal"
-                        onChange={this.handleChange('cityStateZip')}
-                    />
-                    <Button 
-                        className="submit-order" 
-                        variant="raised"
-                        color="primary"
-                        onClick={this.handleSubmit}
-                    >
-                        Order
-                    </Button>
-                </form>
-            </div>
-        );
-    }
+  };
+
+  getMenu = () => {
+    const {restaurant} = this.state;
+    console.log(restaurant);
+    fetch('https://api.eatstreet.com/publicapi/v1/restaurant/' + restaurant.apiKey + '/menu?includeCustomizations=false',
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          'X-Access-Token': '2f6313388bf0a784' // replace with process.env.API_KEY when it works lol
+        },
+
+      }
+    )
+      .then(data => data.json())
+      .then(response => {
+        console.log(response);
+      })
+  };
+
+  render() {
+    return (
+      <div>
+        <form className="order-form" noValidate autoComplete="off">
+          <TextField
+            required
+            id="streetAddress"
+            label="Street Address"
+            margin="normal"
+            onChange={this.handleChange('streetAddress')}
+          />
+          <TextField
+            required
+            id="cityStateZip"
+            label="City, State, Zip Code"
+            margin="normal"
+            onChange={this.handleChange('cityStateZip')}
+          />
+          <Button
+            className="submit-order"
+            variant="raised"
+            color="primary"
+            onClick={this.handleSubmit}
+          >
+            Order
+          </Button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Order;
