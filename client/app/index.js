2bfefc395394e02555951595f6c20cf6609bf64f
Samuel S. Siegmeister <siegmeister.s@husky.neu.edu>
2018-04-07 14:25:15 -0400

    login and signup

--- client/app/index.js
+++ client/app/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from 'react-dom';
+import {render} from 'react-dom';
 
 import {
   BrowserRouter as Router,
@@ -8,23 +8,37 @@ import {
   Switch
 } from 'react-router-dom'
 
+import { Redirect } from 'react-router'
+
 import App from './components/App/App';
 import NotFound from './components/App/NotFound';
 
 import Home from './components/Home/Home';
+import Login from './components/Login';
+import SignUp from './components/SignUp';
 
 import HelloWorld from './components/HelloWorld/HelloWorld';
+import {Provider} from 'react-redux';
+import configureStore from './configureStore';
+
+const store = configureStore();
 
 import './styles/styles.scss';
 
 render((
-  <Router>
-    <App>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/helloworld" component={HelloWorld}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </App>
-  </Router>
+  <Provider store={store}>
+    <Router>
+      <App>
+        <Switch>
+          <Route exact path="/" component={() => {
+            return <Redirect to='/login'/>;
+          }}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/signup" component={SignUp}/>
+          <Route path="/helloworld" component={HelloWorld}/>
+          <Route component={NotFound}/>
+        </Switch>
+      </App>
+    </Router>
+  </Provider>
 ), document.getElementById('app'));
